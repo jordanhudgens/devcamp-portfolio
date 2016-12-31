3da321218bd36f32e2c870b0f7307af6dff7ab90
Jordan Hudgens <jordan@wow.com>
2016-12-31 12:53:37 -0700

    Implemented validations for blogs, skills, and portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,3 @@
 class Portfolio < ApplicationRecord
+  validates_presence_of :title, :body, :main_image, :thumb_image
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,3 @@
 class Skill < ApplicationRecord
+  validates_presence_of :title, :percent_utilized
 end
