1026a764f7a1733b580d6d00f463143cd76a013e
Jordan Hudgens <jordan@wow.com>
2017-01-07 12:40:51 -0700

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
