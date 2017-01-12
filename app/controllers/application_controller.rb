607990147f64c8531eb75a8a88ce05316e319ac9
Jordan Hudgens <jordan@wow.com>
2017-01-11 17:21:55 -0700

    Added guest user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
 end
---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,15 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest User",
+                  first_name: "Guest",
+                  last_name: "User",
+                  email: "guest@example.com"
+                  )
+  end
+end
