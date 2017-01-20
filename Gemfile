38832e7b5c866b353522dce37435f7cc31f34905
Jordan Hudgens <jordan@wow.com>
2017-01-19 18:53:56 -0700

    Implemented partials for blog, font awesome, ang pagination

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 
 gem 'devcamp_view_tool', '~> 0.1.0'
 gem 'petergate', '~> 1.7'
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.1'
+gem 'kaminari', '~> 1.0', '>= 1.0.1'
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
