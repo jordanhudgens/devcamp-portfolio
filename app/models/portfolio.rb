c1695642345d5c4fd7d36eef05681c5b91106c6e
Jordan Hudgens <jordan@wow.com>
2017-01-23 19:54:33 -0700

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,9 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
   def self.angular
     where(subtitle: 'Angular')
   end
---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,13 @@
+class PortfolioUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
