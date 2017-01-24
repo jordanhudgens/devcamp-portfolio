012641c55a3f5e2a5588426ff058cc7440f3b656
Jordan Hudgens <jordan@wow.com>
2017-01-24 14:40:47 -0700

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+  end
+
 end
