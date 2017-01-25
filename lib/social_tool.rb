0402a0b3624e0ff7ed93c71f43bab70d396be633
Jordan Hudgens <jordan@wow.com>
2017-01-25 14:39:45 -0700

    Finalized styles for tech news and updated nav width for app layout

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,7 +7,7 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search("#rubyonrails", result_type: 'recent').take(6).collect do |tweet|
+    client.search("#coding", result_type: 'recent').take(6).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
