8499021400963fd90c595afe412c4943f6b962a3
Jordan Hudgens <jordan@wow.com>
2017-01-27 13:05:33 -0700

    Built out initial form and partial call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,6 +13,9 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
