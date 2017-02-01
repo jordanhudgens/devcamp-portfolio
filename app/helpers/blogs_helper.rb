2915edc51033c573f4163df03de075ce697d02be
Jordan Hudgens <jordan@wow.com>
2017-02-01 13:38:48 -0700

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -22,4 +22,8 @@ module BlogsHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+
+  def blog_status_color blog
+    'color: red;' if blog.draft?
+  end
 end
