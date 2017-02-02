fbb713c42b2959a20fc22f74db4ebe6f9af32118
Jordan Hudgens <jordan@wow.com>
2017-02-02 15:01:28 -0700

    Integrated custom fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
