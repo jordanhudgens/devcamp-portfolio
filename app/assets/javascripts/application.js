bb00e969a5c4a433f41a70de8feb13965d1029af
Jordan Hudgens <jordan@wow.com>
2017-02-03 13:05:38 -0700

    Fixed JS loading bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
@@ -20,3 +19,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
