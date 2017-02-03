5248ded8d6617a182d6acc00f03cbe21db815f7b
Jordan Hudgens <jordan@wow.com>
2017-02-03 12:22:25 -0700

    Updated production file to take in rails_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.web_socket_server_url = 'wss://devcamp-portfolio.herokuapp.com/cable'
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   config.action_cable.allowed_request_origins = ['https://devcamp-portfolio.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
