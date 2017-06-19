d9ac59bad4e94b57160610617dfc83560657564d
Jordan Hudgens <jordan@wow.com>
2017-06-19 10:01:27 -0700

    Implemented the typed.js library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,8 @@
 //= require html.sortable
 //= require cocoon
 //= require gritter
+//= require lib/typed
+//= require typed-implementation
 //= require cable
 //= require turbolinks
 //= require_tree .
