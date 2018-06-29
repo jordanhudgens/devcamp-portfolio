9af1b7ba85bf131825eaddad5c5c26aeaa3d4da8
Jordan Hudgens <jordan@devcamp.com>
2018-06-28 22:41:22 -0500

    Added update for the language helper for code issues

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -5,6 +5,7 @@ module BlogsHelper
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
+      language ||= :plaintext
       CodeRay.scan(code, language).div
     end
   end
