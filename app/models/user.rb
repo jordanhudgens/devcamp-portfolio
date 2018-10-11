e30333c3b95b9a9a5352082314fabf0be3c77e03
jordanhudgens <jordan@devcamp.com>
2018-10-10 19:15:57 -0500

    Added demo roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:site_admin], multiple: false)                                      ##
+  petergate(roles: [:site_admin, :demo_admin, :third_admin], multiple: true)                ##
   ############################################################################################ 
  
 
